160081329fcf997bf2a7c3b6c82d7f6f0297c11f
Pablo García Miranda <uo245530@uniovi.es>
2018-02-07 16:53:59 +0100

    Dockers communicating between them with auth enabled in db.
    Simple API created for testing the communication between containers.
    docker-compose.yml edited. Volume for db added and data to connect to db extracted to variables in an .env file.

---
+++ express-server/api.js
@@ -0,0 +1,68 @@
+// Import dependencies
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+//Auth credentials to connect to mongo
+const options = {
+  user: process.env['MONGODB_ADMIN_USER'],
+  pass: process.env['MONGODB_ADMIN_PASS']
+};
+
+// 'database' is the service specified in the docker-compose file.
+const dbHost = 'mongodb://database/admin';
+
+
+// Connect to mongodb
+mongoose.connect(dbHost, options);
+
+// create mongoose schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+// create mongoose model
+const User = mongoose.model('User', userSchema);
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+        console.log(res)
+        res.send('api works');
+});
+
+/* GET all users. */
+router.get('/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) res.status(500).send(error)
+
+        res.status(200).json(users);
+    });
+});
+
+/* GET one users. */
+router.get('/users/:id', (req, res) => {
+    User.findById(req.param.id, (err, users) => {
+        if (err) res.status(500).send(error)
+
+        res.status(200).json(users);
+    });
+});
+
+/* Create a user. */
+router.post('/users', (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    user.save(error => {
+        if (error) res.status(500).send(error);
+
+        res.status(201).json({
+            message: 'User created successfully'
+        });
+    });
+});
+
+module.exports = router;
--- express-server/server.js
+++ express-server/server.js
@@ -1,16 +1,42 @@
 'use strict';
 
+// Get dependencies
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
+// Get our API routes
+const api = require('./api');
 
-// App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world, Pablo\n');
-});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Cross Origin middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+// Set our api routes
+app.use('/', api);
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
